f5ae020bc2cfa46fff650da31c809b6937e9a62a
rj02sd <sevadusk@gmail.com>
2023-06-12 19:18:58 -0400

    Quick updates

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 200.0
 var shrines
 var shrine_type = "default"
 
@@ -61,18 +61,22 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	var direction = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down"))
 
+	var targetVelocity = Vector2.ZERO
+	
 	if direction.length() > 1:
 		direction = direction.normalized()
-
+		
 	if direction.x:
-		velocity.x = direction.x * SPEED
+		targetVelocity.x = direction.x * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		targetVelocity.x = move_toward(velocity.x, 0, SPEED)
 
 	if direction.y:
-		velocity.y = direction.y * SPEED
+		targetVelocity.y = direction.y * SPEED
 	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		targetVelocity.y = move_toward(velocity.y, 0, SPEED)
+
+	velocity = velocity.lerp(targetVelocity, 0.03)
 	
 	if velocity.length() > 0:
 		rotation = -velocity.angle_to(Vector2(1, 0))
--- scripts/teleporter.gd
+++ scripts/teleporter.gd
@@ -14,16 +14,15 @@ func _on_body_entered(body):
 	match get_node("../").get_meta("teleport_location"):
 		1:
 			get_tree().get_nodes_in_group("Player")[0].position.x = get_tree().get_nodes_in_group("loc1")[0].position.x
-			get_tree().get_nodes_in_group("Player")[0].position.y = get_tree().get_nodes_in_group("loc1")[0].position.y+100
+			get_tree().get_nodes_in_group("Player")[0].position.y = get_tree().get_nodes_in_group("loc1")[0].position.y+200
 		2:
 			get_tree().get_nodes_in_group("Player")[0].position.x = get_tree().get_nodes_in_group("loc2")[0].position.x
-			get_tree().get_nodes_in_group("Player")[0].position.y = get_tree().get_nodes_in_group("loc2")[0].position.y-100
+			get_tree().get_nodes_in_group("Player")[0].position.y = get_tree().get_nodes_in_group("loc2")[0].position.y+200
 		3:
 			get_tree().get_nodes_in_group("Player")[0].position.x = get_tree().get_nodes_in_group("loc3")[0].position.x
-			get_tree().get_nodes_in_group("Player")[0].position.y = get_tree().get_nodes_in_group("loc3")[0].position.y+100
+			get_tree().get_nodes_in_group("Player")[0].position.y = get_tree().get_nodes_in_group("loc3")[0].position.y+200
 		4:
 			get_tree().get_nodes_in_group("Player")[0].position.x = get_tree().get_nodes_in_group("loc4")[0].position.x
-			get_tree().get_nodes_in_group("Player")[0].position.y = get_tree().get_nodes_in_group("loc4")[0].position.y-100
+			get_tree().get_nodes_in_group("Player")[0].position.y = get_tree().get_nodes_in_group("loc4")[0].position.y+200
 		5:
-			var simultaneous_scene = preload("res://throne_room.tscn").instantiate()
-			get_tree().change_scene_to_file("res://throne_room.tscn")
+			get_tree().change_scene_to_file("res://scenes/throne_room.tscn")
